e67cea6b83c7576139779b3069cdc0078eb7eafe
James Totterdell <jatotterdell@gmail.com>
2022-10-27 10:07:02 +1100

    Add creatinine_clearance

--- R/utils.R
+++ R/utils.R
@@ -48,6 +48,22 @@ findfirst <- function(x, v = NA) {
 }
 
 
+#' @title creatinine_clearance
+#' @description
+#' Serum creatinine clearance by Cockcroft-Gault formula
+#' https://www.eviq.org.au/clinical-resources/eviq-calculators/3200-creatinine-clearance-calculator
+#' @param sex Either "Male" or "Female"
+#' @param age Age in years
+#' @param weight Weight in kg
+#' @param creatinine Serum creatinine in umol/L
+#' @return Creatinine clearance rate in mL/min.
+creatinine_clearance <- function(sex, age, weight, creatinine) {
+  out <- (140 - age) * weight / (0.814 * 72 * creatinine)
+  out[sex == "Female"] <- 0.85 * out[sex == "Female"]
+  return(out)
+}
+
+
 
 # Save outputs ----
 
